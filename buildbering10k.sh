6d2fae88f062150e4ab5b67dca7b066f5c139865
K. Kearney <kellyke8@mox2.hyak.local>
2018-01-30 11:56:20 -0800

    Updated build script to that from kakoverhaul branch

--- buildbering10k.sh
+++ buildbering10k.sh
@@ -3,6 +3,90 @@
 # on either cluster1 or beast.  It compiles 3 separate variants: 
 # physics-only, bestnpz, and feast.  
 #
+# See useage statement for syntax.
+
+
+USEAGE="Usage: buildbering10k [-s <suffix>] [-e <epath>] [-pPnNfF] [-h]
+
+where:
+
+  -s suffix:  add suffix string to the end of the ocean[M/G] 
+              executables. Useful if compiling a version based on a 
+              branch without wanting to overwrite the master-compiled 
+              version.  If not included, the default names (oceanM_phys, 
+              oceanM_npz, and oceanM_feast) will be used.
+
+  -e epath:   path to folder where compiled executables should be 
+              placed.  Default is ../romsexecs/
+
+  -p:         compile physics-only version
+
+  -P:         compile physics-only version in debug mode
+
+  -n:         compile BEST_NPZ version
+
+  -N:         compile BEST_NPZ version in debug mode
+
+  -f:         compile FEAST version
+
+  -F:         compile FEAST version in debug mode
+  
+  -h:         show this help text
+"
+
+if [[ $# -eq 0 ]] ; then
+    echo "This script now requires arguments:
+
+$USEAGE"
+    exit 0
+fi
+
+pfile="oceanM_phys"
+nfile="oceanM_npz"
+ffile="oceanM_feast"
+Pfile="oceanG_phys"
+Nfile="oceanG_npz"
+Ffile="oceanG_feast"
+
+epath="../romsexecs/"
+
+pflag=false
+nflag=false
+fflag=false
+Pflag=false
+Nflag=false
+Fflag=false
+
+while getopts ":s:e:pPnNfFh" opt; do
+  case $opt in
+    s) pfile="oceanM_phys_${OPTARG}"
+       nfile="oceanM_npz_${OPTARG}"
+       ffile="oceanM_feast_${OPTARG}"
+       Pfile="oceanG_phys_${OPTARG}"
+       Nfile="oceanG_npz_${OPTARG}"
+       Ffile="oceanG_feast_${OPTARG}"
+      ;;
+    e) epath=$OPTARG;;
+    p) pflag=true;;
+    P) Pflag=true;;
+    n) nflag=true;;
+    N) Nflag=true;;
+    f) fflag=true;;
+    F) Fflag=true;;
+    h) echo "$USEAGE"
+       exit
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$USEAGE" >&2
+       exit 1
+       ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      echo "$USEAGE" >&2
+      exit 1
+      ;;
+  esac
+done
 
 #--------------
 # Setup
@@ -10,7 +94,7 @@
 
 # Things that are the same on all computers
 
-export   ROMS_APPLICATION=NEP5 
+export   ROMS_APPLICATION=NEP5
 export        NestedGrids=1
 export        MY_ROOT_DIR=${PWD}                  # Location of roms-bering-sea clone
 export     MY_PROJECT_DIR=${MY_ROOT_DIR}/Apps/NEP # Location of analyticals, headers, etc
@@ -29,19 +113,29 @@ case $HOSTNAME in
     export            PATH=/usr/mpi/intel/openmpi-1.4.1/bin:$PATH  # mpif90 location
     export   NETCDF_INCDIR=/home/aydink/includeM                   # netcdf include
     export   NETCDF_LIBDIR=/home/aydink/libM                       # netcdf lib
-	;; 
+    
+    source /opt/intel/Compiler/11.1/069/bin/ifortvars.sh intel64 # TODO: What does this do?
+    
+  ;;
   afsc-s29.afsc.noaa.gov) # cluster1
     export            PATH=/opt/intel/openmpi/163/bin:$PATH
     export   NETCDF_INCDIR=/home/aydink/include                    # netcdf include
     export   NETCDF_LIBDIR=/home/aydink/lib                        # netcdf lib
+    
+    source /opt/intel/Compiler/11.1/069/bin/ifortvars.sh intel64 # TODO: What does this do?
+    
     ;;
-  *)   
+  mox[12].hyak.local) # hyak-mox
+    export   PATH=/gscratch/sw/intel-201703/compilers_and_libraries_2017.2.174/linux/mpi/intel64/bin/mpif90:$PATH
+    export   NETCDF_INCDIR=/sw/netcdf-fortran+c-4.4.1.1_icc-17/include # netcdf include
+    export   NETCDF_LIBDIR=/sw/netcdf-fortran+c-4.4.1.1_icc-17/lib     # netcdf lib
+    ;;
+  *)
     echo "Not set up to compile on this computer"
     exit 1
     ;;
 esac
 
-source /opt/intel/Compiler/11.1/069/bin/ifortvars.sh intel64 # TODO: What does this do?
 
 # These variables are based on those set above
 
@@ -53,56 +147,131 @@ export            BINDIR=${MY_ROOT_DIR}     # Where the compiled program goes
 # Make
 #--------------
 
+rbsbranch=$(git symbolic-ref -q --short HEAD)
+compdate=$(date)
+revis=$(git rev-parse HEAD)
+
 # Compile physics
 
-export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build1
+export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build_phys
+export      USE_DEBUG=
 
-make clean &>/dev/null
-echo "Compiling physics-only variant"
-make -j &> buildouterr.txt
-if [ $? -ne 0 ]; then
-	mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+if [ "$pflag" = true ]; then
+  make clean &>/dev/null
+  echo "Compiling physics-only variant"
+  make -j &> buildouterr.txt
+  if [ $? -ne 0 ]; then
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
     echo "  Compilation failed: see ${SCRATCH_DIR}/buildouterr.txt for details"
-else
-	mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
-    mv oceanM oceanM_phys
-	echo "  Success: oceanM_phys created"
+  else
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+    mv oceanM ${epath%%/}/${pfile}
+    echo "  Success: $pfile created"
+    echo "$pfile,$compdate,$rbsbranch,$revis,${MY_CPP_FLAGS}" >> ${epath%%/}/buildbering.log
+  fi
+fi
+
+# Compile physics (debugging)
+
+export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build_physdb
+export      USE_DEBUG=on
+
+if [ "$Pflag" = true ]; then
+  make clean &>/dev/null
+  echo "Compiling physics-only (debugging) variant"
+  make -j &> buildouterr.txt
+  if [ $? -ne 0 ]; then
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+    echo "  Compilation failed: see ${SCRATCH_DIR}/buildouterr.txt for details"
+  else
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+    mv oceanG ${epath%%/}/${Pfile}
+    echo "  Success: $Pfile created"
+    echo "$Pfile,$compdate,$rbsbranch,$revis,${MY_CPP_FLAGS}" >> ${epath%%/}/buildbering.log
+  fi
 fi
 
 # Compile bestnpz
 
-export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build2
-export      MY_CPP_FLAGS="-DBEST_NPZ"
+export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build_npz
+export      USE_DEBUG=
+export      MY_CPP_FLAGS="${MY_CPP_FLAGS} -DBEST_NPZ"
 
-make clean &>/dev/null
-echo "Compiling bestnpz variant"
-make -j &> buildouterr.txt
-if [ $? -ne 0 ]; then
-	mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
-    echo "  Compilation failed: see ${SCRATCH_DIR}/buildouterr.txt for details"
-else
-	mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
-    mv oceanM oceanM_npz
-	echo "  Success: oceanM_npz created"
+if [ "$nflag" = true ]; then
+  make clean &>/dev/null
+  echo "Compiling bestnpz variant"
+  make -j &> buildouterr.txt
+  if [ $? -ne 0 ]; then
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+      echo "  Compilation failed: see ${SCRATCH_DIR}/buildouterr.txt for details"
+  else
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+      mv oceanM ${epath%%/}/${nfile}
+    echo "  Success: $nfile created"
+    echo "$nfile,$compdate,$rbsbranch,$revis,${MY_CPP_FLAGS}" >> ${epath%%/}/buildbering.log
+  fi
+fi
+
+# Compile bestnpz (debugging)
+
+export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build_npzdb
+export      USE_DEBUG=on
+
+if [ "$Nflag" = true ]; then
+  make clean &>/dev/null
+  echo "Compiling bestnpz (debugging) variant"
+  make -j &> buildouterr.txt
+  if [ $? -ne 0 ]; then
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+      echo "  Compilation failed: see ${SCRATCH_DIR}/buildouterr.txt for details"
+  else
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+    mv oceanG ${epath%%/}/${Nfile}
+    echo "  Success: $Nfile created"
+    echo "$Nfile,$compdate,$rbsbranch,$revis,${MY_CPP_FLAGS}" >> ${epath%%/}/buildbering.log
+  fi
 fi
 
 # Compile Feast
 
-export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build3
+export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build_feast
 export      MY_CPP_FLAGS="${MY_CPP_FLAGS} -DFEAST"
+export      USE_DEBUG=
 
-make clean &>/dev/null
-echo "Compiling feast variant"
-make -j &> buildouterr.txt
-if [ $? -ne 0 ]; then
-	mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
-    echo "  Compilation failed: see ${SCRATCH_DIR}/buildouterr.txt for details"
-else
-	mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
-    mv oceanM oceanM_feast
-	echo "  Success: oceanM_feast created"
+if [ "$fflag" = true ]; then
+  make clean &>/dev/null
+  echo "Compiling feast variant"
+  make -j &> buildouterr.txt
+  if [ $? -ne 0 ]; then
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+      echo "  Compilation failed: see ${SCRATCH_DIR}/buildouterr.txt for details"
+  else
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+      mv oceanM ${epath%%/}/${ffile}
+    echo "  Success: $ffile created"
+    echo "$ffile,$compdate,$rbsbranch,$revis,${MY_CPP_FLAGS}" >> ${epath%%/}/buildbering.log
+  fi
 fi
 
+# Compile Feast (debugging)
+
+export       SCRATCH_DIR=${MY_PROJECT_DIR}/Build_feastdb
+export      USE_DEBUG=on
+
+if [ "$Fflag" = true ]; then
+  make clean &>/dev/null
+  echo "Compiling feast (debugging) variant"
+  make -j &> buildouterr.txt
+  if [ $? -ne 0 ]; then
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+      echo "  Compilation failed: see ${SCRATCH_DIR}/buildouterr.txt for details"
+  else
+    mv buildouterr.txt ${SCRATCH_DIR}/buildouterr.txt
+    mv oceanG ${epath%%/}/${Ffile}
+    echo "  Success: $Ffile created"
+    echo "$Ffile,$compdate,$rbsbranch,$revis,${MY_CPP_FLAGS}" >> ${epath%%/}/buildbering.log
+  fi
+fi
 
 
 
