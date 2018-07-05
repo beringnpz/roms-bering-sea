19b903d8cc492d07cff60544b80a4af09f47fedd
K. Kearney <kellyke8@mox2.hyak.local>
2018-07-05 13:56:16 -0700

    Updated scratch compilation script

--- compilelots.sh
+++ compilelots.sh
@@ -1,7 +1,9 @@
 # ./buildbering10k.sh -n -s PIvavg
 
-export MY_CPP_FLAGS="-DGPPMID"
-./buildbering10k.sh -n -s PIvmid
-
+./buildbering10k.sh -n -s PIvGPPavg
 export MY_CPP_FLAGS="-DPI_CONSTANT"
-./buildbering10k.sh -n -s PIcavg
+./buildbering10k.sh -n -s PIcGPPavg
+export MY_CPP_FLAGS="-DPI_CONSTANT -DGPPMID"
+./buildbering10k.sh -n -s PIcGPPmid
+export MY_CPP_FLAGS="-DGPPMID"
+./buildbering10k.sh -n -s PIvGPPmid
