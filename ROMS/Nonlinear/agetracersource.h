716a80bbe53f4529bc881a1d2364f918858a2443
Kelly <kakearney@gmail.com>
2019-11-01 10:56:10 -0700

    Moving age tracer source out of bestnpz and to its own biology_tile function

---
+++ ROMS/Nonlinear/agetracersource.h
@@ -0,0 +1,195 @@
+      SUBROUTINE biology (ng,tile)
+!
+!svn $Id: npzd_Franks.h 1075 2009-09-25 22:41:13Z kate $
+!************************************************** Hernan G. Arango ***
+!  Copyright (c) 2002-2009 The ROMS/TOMS Group        Craig V. Lewis   !
+!    Licensed under a MIT/X style license                              !
+!    See License_ROMS.txt                                              !
+!***********************************************************************
+!                                                                      !
+!  Nutrient-Phytoplankton-Zooplankton-Detritus Model.                  !
+!                                                                      !
+!  This routine computes the biological sources and sinks and adds     !
+!  then the global biological fields.                                  !
+!                                                                      !
+!  Reference:                                                          !
+!                                                                      !
+!    Franks et al, 1986: Behavior of simple plankton model with        !
+!      food-level acclimation by herbivores, Marine Biology, 91,       !
+!      121-129.                                                        !
+!                                                                      !
+!  Adapted from code written originally by Craig V. Lewis.             !
+!                                                                      !
+!***********************************************************************
+!
+      USE mod_param
+      USE mod_grid
+      USE mod_ncparam
+      USE mod_ocean
+      USE mod_stepping
+!
+!  Imported variable declarations.
+!
+      integer, intent(in) :: ng, tile
+!
+!  Local variable declarations.
+!
+#include "tile.h"
+!
+!  Set header file name.
+!
+#ifdef DISTRIBUTE
+      IF (Lbiofile(iNLM)) THEN
+#else
+      IF (Lbiofile(iNLM).and.(tile.eq.0)) THEN
+#endif
+        Lbiofile(iNLM)=.FALSE.
+        BIONAME(iNLM)=__FILE__
+      END IF
+!
+#ifdef PROFILE
+      CALL wclock_on (ng, iNLM, 15)
+#endif
+      CALL biology_tile (ng, tile,                                      &
+     &                   LBi, UBi, LBj, UBj, N(ng), NT(ng),             &
+     &                   IminS, ImaxS, JminS, JmaxS,                    &
+     &                   nstp(ng), nnew(ng),                            &
+#ifdef MASKING
+     &                   GRID(ng) % rmask,                              &
+#endif
+     &                   GRID(ng) % Hz,                                 &
+     &                   GRID(ng) % z_r,                                &
+     &                   GRID(ng) % z_w,                                &
+     &                   OCEAN(ng) % t)
+
+#ifdef PROFILE
+      CALL wclock_off (ng, iNLM, 15)
+#endif
+      RETURN
+      END SUBROUTINE biology
+!
+!-----------------------------------------------------------------------
+      SUBROUTINE biology_tile (ng, tile,                                &
+     &                         LBi, UBi, LBj, UBj, UBk, UBt,            &
+     &                         IminS, ImaxS, JminS, JmaxS,              &
+     &                         nstp, nnew,                              &
+#ifdef MASKING
+     &                         rmask,                                   &
+#endif
+     &                         Hz, z_r, z_w,                            &
+     &                         t)
+!-----------------------------------------------------------------------
+!
+      USE mod_param
+      USE mod_biology
+      USE mod_ncparam
+      USE mod_scalars
+!
+!  Imported variable declarations.
+!
+      integer, intent(in) :: ng, tile
+      integer, intent(in) :: LBi, UBi, LBj, UBj, UBk, UBt
+      integer, intent(in) :: IminS, ImaxS, JminS, JmaxS
+      integer, intent(in) :: nstp, nnew
+
+#ifdef ASSUMED_SHAPE
+# ifdef MASKING
+      real(r8), intent(in) :: rmask(LBi:,LBj:)
+# endif
+      real(r8), intent(in) :: Hz(LBi:,LBj:,:)
+      real(r8), intent(in) :: z_r(LBi:,LBj:,:)
+      real(r8), intent(in) :: z_w(LBi:,LBj:,0:)
+      real(r8), intent(inout) :: t(LBi:,LBj:,:,:,:)
+#else
+# ifdef MASKING
+      real(r8), intent(in) :: rmask(LBi:UBi,LBj:UBj)
+# endif
+      real(r8), intent(in) :: Hz(LBi:UBi,LBj:UBj,UBk)
+      real(r8), intent(in) :: z_r(LBi:UBi,LBj:UBj,UBk)
+      real(r8), intent(in) :: z_w(LBi:UBi,LBj:UBj,0:UBk)
+      real(r8), intent(inout) :: t(LBi:UBi,LBj:UBj,UBk,3,UBt)
+#endif
+!
+!  Local variable declarations.
+!
+      integer, parameter :: Nsink = 1
+
+      integer :: Iter, i, ibio, isink, itrc, itrmx, j, k, ks
+
+      integer, dimension(Nsink) :: idsink
+
+      real(r8), parameter :: eps = 1.0e-16_r8
+
+      real(r8) :: cff, cff1, cff2, cff3, dtdays
+      real(r8) :: cffL, cffR, cu, dltL, dltR
+
+      real(r8), dimension(Nsink) :: Wbio
+
+      integer, dimension(IminS:ImaxS,N(ng)) :: ksource
+
+      real(r8), dimension(IminS:ImaxS,N(ng),NT(ng)) :: Bio
+
+      real(r8), dimension(IminS:ImaxS,N(ng),NT(ng)) :: Bio_bak
+
+      real(r8), dimension(IminS:ImaxS,0:N(ng)) :: FC
+
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: Hz_inv
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: Hz_inv2
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: Hz_inv3
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: WL
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: WR
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: bL
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: bR
+      real(r8), dimension(IminS:ImaxS,N(ng)) :: qc
+
+#include "set_bounds.h"
+!
+!-----------------------------------------------------------------------
+!  Add biological Source/Sink terms.
+!-----------------------------------------------------------------------
+!
+!  Set time-stepping according to the number of iterations.
+!
+      dtdays=dt(ng)*sec2day/REAL(BioIter(ng),r8)
+
+!
+!  Compute inverse thickness to avoid repeated divisions.
+!
+      J_LOOP : DO j=Jstr,Jend
+
+!
+!-----------------------------------------------------------------------
+!  Update global tracer variables (m Tunits).
+!-----------------------------------------------------------------------
+!
+
+        DO k=1,N(ng)
+          DO i=Istr,Iend
+            
+            if (((j .eq. 57) .and. (i .eq. 75)) .or. ((j .eq. 82) .and. (i .eq. 45))) then
+              itrc = inert(1)
+              t(i,j,k,nnew,itrc) = t(i,j,k,nnew,itrc) + 5.0_r8*Hz(i,j,k)*dtdays 
+            endif
+
+            ! Dye tracer 3-4: Along shelf break source
+
+            if ((z_w(i,j,0) .lt. -250.0_r8) .and.  (z_w(i,j,0) .gt. -350.0_r8)) then
+              itrc = inert(3)
+              t(i,j,k,nnew,itrc) = t(i,j,k,nnew,itrc) + 5.0_r8*Hz(i,j,k)*dtdays
+            endif
+
+            ! Dye tracer 5-6: M2 source
+
+            if ((j .eq. 62) .and. (i .eq. 99)) then
+              itrc = inert(5)
+              t(i,j,k,nnew,itrc) = t(i,j,k,nnew,itrc) + 5.0_r8*Hz(i,j,k)*dtdays 
+            endif
+
+          END DO
+        END DO
+
+
+      END DO J_LOOP
+
+      RETURN
+      END SUBROUTINE biology_tile
