b59e237b498a5facc8e73eb3ff7f8001603730f6
Kelly A Kearney <kellyke8@klone-login03.hyak.local>
2025-02-20 10:15:11 -0800

    Fixed bug where # of diagnostics was incorrect for no-CARBON option

--- ROMS/Include/bestnpz_mod_param_NBTS.h
+++ ROMS/Include/bestnpz_mod_param_NBTS.h
@@ -1,5 +1,7 @@
 #  ifdef FEAST
-        integer, parameter :: NBTS = 155
-#  else
+        integer, parameter :: NBTS = 155 ! May not be able to do FEAST w/o CARBON?
+#  elif defined CARBON
         integer, parameter :: NBTS = 150
+#  else
+        integer, parameter :: NBTS = 148
 #  endif
